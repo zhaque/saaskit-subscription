456b67643e7083c655fbf925a4ab0096bbecfbfe
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-11-13 02:33:10 +0200

    change behaviour of middleware. It caughts exception, then redirects.

--- subscription/middleware.py
+++ subscription/middleware.py
@@ -7,12 +7,8 @@ from django.conf import settings
 from subscription.models import UserSubscription
 
 class SubscriptionMiddleware(object):
-    
-    def process_request(self, request):
-        if request.user.is_authenticated() \
-        and not request.path.startswith(reverse('subscription_list')) \
-        and not request.path.startswith(settings.MEDIA_URL):
-            try:
-                sub = request.user.subscription
-            except UserSubscription.DoesNotExist:
-                return redirect('subscription_list')
+
+    def process_exception(self, request, exception):
+        if type(exception) == UserSubscription.DoesNotExist:
+            return redirect('subscription_list')
+        
--- subscription/tests/__init__.py
+++ subscription/tests/__init__.py
@@ -1,7 +1,8 @@
 ### -*- coding: utf-8 -*- ####################################################
 
-from test_client import ClientTest
+#from test_client import ClientTest
 from test_model import ModelTest
+from test_middleware import MiddlewareTest
 #from test_admin import AdminTest
 
 from paypal.standard.ipn.tests.test_ipn import IPNTest
