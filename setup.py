73aaab1439bae9f3460bdf89ac101f9567abdcdf
SaaSkit <saaskit@DevBook.home>
2009-12-22 16:17:30 +0000

    rename package and fix license

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(name="saaskit-subscription",
            version="0.1",
            description="Subscriptions based web app using django-paypal",
-           author="Arvid Paeglit",
-           author_email="arvi3d@gmail.com",
+           author="SaaS kit",
+           author_email="admin@saaskit.org",
            packages=find_packages(),
            include_package_data=True,
 )
