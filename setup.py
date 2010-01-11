60228a75228ad7e5a63653d2a7ba589a9270d5bd
CrowdSense <admin@DevBook.home>
2010-01-11 07:17:47 +0000

    fixed naming convention

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name="saaskit-subscription",
            version="0.1",
            description="Subscriptions based web app using django-paypal",
-           author="SaaS kit",
+           author="SaaSkit",
            author_email="admin@saaskit.org",
            packages=find_packages(),
            include_package_data=True,
